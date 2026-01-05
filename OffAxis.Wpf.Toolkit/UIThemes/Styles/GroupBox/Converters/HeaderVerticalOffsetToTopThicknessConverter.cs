b9142d462b69ac39f531a417de01976b72f5e3ff
DillyGoatGruff <dschweiss@outlook.com>
2026-01-05 14:45:32 -0600

    Added margin to the control equal to the vertical offset of the header. Added padding to the contents equal to the header offset.

---
+++ OffAxis.Wpf.Toolkit/UIThemes/Styles/GroupBox/Converters/HeaderVerticalOffsetToTopThicknessConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace OffAxis.Wpf.Toolkit.UIThemes.Styles.GroupBox.Converters
+{
+    internal class HeaderVerticalOffsetToTopThicknessConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is Thickness margin)
+            {
+                // The header is vertically shifted down by the Top margin, so to offset that we need to move it up by that amount
+                return new Thickness(0, -margin.Top, 0,0);
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
